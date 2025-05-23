953adb8389bdf839fda6fc6f2d5630fb3797f575
Vera Kippel <veki2400@student.miun.se>
2025-05-23 22:08:29 +0200

    lägger in cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 app.use(bodyParser.json());
 
 //Routes
