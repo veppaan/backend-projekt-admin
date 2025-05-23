773bade92e0a2739972ba630d3c72cd3370b0607
Vera Kippel <veki2400@student.miun.se>
2025-05-23 15:57:10 +0200

    gör validering för användarnamn och lösenord

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,36 @@ const router = express.Router();
 
 //Add new user
 router.post("/register", async(req, res) => {
-    console.log("Register user test")
+    try{
+        const { username, password } = req.body;
+
+        let errors = [];
+
+        //Validate input
+        if(username.length < 7){
+            errors.push("Too short username");
+        }
+        if(password.length < 7){
+            errors.push("Too short password");
+        }
+        if(!/[a-z]/.test(password)){
+            errors.push("Password must contain lowercase letters (a-z)");
+        }
+        if(!/[A-Z]/.test(password)){
+            errors.push("Password must contain uppercase letters (A-Z)");
+        }
+        if(!/[0-9]/.test(password)){
+            errors.push("Password must contain digits");
+        }
+        if(errors !== 0){
+            return errors;
+        }else {
+
+        }
+
+    }catch(error){
+        res.status(500).json({ error: "Server error" });
+    }
 })
 
 //Login user
